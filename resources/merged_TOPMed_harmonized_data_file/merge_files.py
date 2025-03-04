94df5b95ea531d5c82a3ea6aa8e85e93d389c722
Sigfried Gold <sigfried@sigfried.org>
2025-03-04 12:50:20 -0500

    Code for generating dbgap_priority_only.csv (in merge_files.py)
    - Also updated README.md
    - A couple csv files have changes because I regenerated them for the
      first time since adding COPDGene rows to dbgap_variables_priority_cohorts

--- resources/merged_TOPMed_harmonized_data_file/merge_files.py
+++ resources/merged_TOPMed_harmonized_data_file/merge_files.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import re
 import os
+from resources.load_data import load_csv
 
 
 def extract_phv(text):
@@ -11,17 +12,17 @@ def extract_phv(text):
 
 def merge_files():
     # Read the CSVs
-    dbgap_vars = pd.read_csv('./copies_of_external_source_files/dbgap_variables_priority_cohorts.csv')
-    raw_to_harm = pd.read_csv('./map-topmed-raw-to-harmonized/raw-to-harmonized-topmed-vars.csv')
+    dbgap_priority = load_csv('dbgap_priority')
+    raw_to_harm = load_csv('topmed_raw_to_harm')
 
     # Extract PHV numbers from the 'Variable accession' column
-    dbgap_vars['phv_number'] = dbgap_vars['Variable accession'].apply(extract_phv)
+    dbgap_priority['phv_number'] = dbgap_priority['Variable accession'].apply(extract_phv)
 
     # Extract PHV numbers from the 'raw' column
     raw_to_harm['phv_number'] = raw_to_harm['raw'].apply(extract_phv)
 
     # Verify uniqueness of PHV numbers in both dataframes
-    if len(dbgap_vars['phv_number'].unique()) != len(dbgap_vars['phv_number']):
+    if len(dbgap_priority['phv_number'].unique()) != len(dbgap_priority['phv_number']):
         print("Warning: Duplicate PHV numbers found in ./copies_of_external_source_files/dbgap_variables_priority_cohorts.csv")
 
     # if len(raw_to_harm['phv_number'].unique()) != len(raw_to_harm['phv_number']):
@@ -29,7 +30,7 @@ def merge_files():
 
     # Merge dataframes on PHV number
     merged_df = pd.merge( raw_to_harm,
-                          dbgap_vars,
+                          dbgap_priority,
                           on='phv_number',
                           how='inner')
 
@@ -77,15 +78,21 @@ def merge_files():
     script_path = os.path.dirname(os.path.abspath(__file__))
     merged_df_pretty.to_csv(os.path.join(script_path, 'merged_variables.csv'), index=False)
 
-    # anti-join, raw_to_harm but not in dbgap_vars
-    outer = raw_to_harm.merge(dbgap_vars, how='outer', indicator=True, on='phv_number')
+    # anti-join, raw_to_harm but not in dbgap_priority
+    outer = raw_to_harm.merge(dbgap_priority, how='outer', indicator=True, on='phv_number')
     outer = outer[[*raw_to_harm.columns, '_merge']]
     raw_to_harm_only = outer[(outer._merge=='left_only')].drop('_merge', axis=1)
     # raw_to_harm_only = raw_to_harm_only.rename(columns=column_mapping)
-
     output_path = os.path.join(script_path, 'raw_to_harm_only.csv')
-
     raw_to_harm_only.to_csv(output_path, index=False)
+
+    # anti-join, dbgap_priority but not in raw_to_harm
+    outer = dbgap_priority.merge(raw_to_harm, how='outer', indicator=True, on='phv_number')
+    outer = outer[[*dbgap_priority.columns, '_merge']]
+    dbgap_priority_only = outer[(outer._merge=='left_only')].drop('_merge', axis=1)
+    # dbgap_priority_only = dbgap_priority_only.rename(columns=column_mapping)
+    output_path = os.path.join(script_path, 'dbgap_priority_only.csv')
+    dbgap_priority_only.to_csv(output_path, index=False)
 
     pass
 
